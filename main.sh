afe18f24dbe2a3d9b1dbb118fdc89ab228364117
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-11 22:36:03 +0300

    test function

--- main.sh
+++ main.sh
@@ -1,15 +1,63 @@
 #!/bin/bash
-filename=main.sh
-function search()
-{
-  local search=$1
-  #local line_num=1
-  while read -r line; do
-    echo "$line"
-    grep "$search"
+filename=$1
 
-    #line_num=$((line_num+1))
-    done < "$filename"
+
+#Author Dima
+#
+#function search()
+#{
+#  local string=$1
+#  for i in $filename; do
+#  grep -i "$string*" $filename; sort -d
+#
+#  done
+#local string=$1
+#  local line_num=1
+#  while IFS= read -r line; do
+#    grep -i "$string*" ; sort -d
+#    line_num=$((line_num+1))
+#    done < "$filename"
+#    echo $@
+#
+#
+#}
+
+
+filename=$1
+#1. function gets a string as argument
+#2. read the $filename global variable and find all the
+#And return
+function main()
+{
+    local string=$1
+    local result=""
+    local array=()
+
+    while IFS= read -r
+        do
+        result=$(grep -i "$string")
+        array+=("$result")
+        done < "$filename"
+
+#    for value in ${array[*]}
+#    do
+#        echo "$value"
+#    done
+    echo "${array[*]}"
+
 }
 
-search echo
+var="$(main rv)"
+echo $var
+main "rv"
+#
+#index=0
+#while read line; do
+#    array[$index]="$line"
+#    index=$(($index+1))
+#done < filename
+#
+#for ((a=0; a < ${#array[*]}; a++))
+#do
+#    echo "$a: ${array[$a]}"
+#done
