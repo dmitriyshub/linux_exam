b317ddf13ad713bf497f920358426c2328514441
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-11 17:04:46 +0300

    test function

--- main.sh
+++ main.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+filename=main.sh
+function search()
+{
+  local search=$1
+  #local line_num=1
+  while read -r line; do
+    echo "$line"
+    grep "$search"
 
-#git test
+    #line_num=$((line_num+1))
+    done < "$filename"
+}
+
+search echo
