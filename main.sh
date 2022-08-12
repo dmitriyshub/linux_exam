33455be6ff70dd0a9efdc0cc26dd33080e5e58a0
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-12 14:32:57 +0300

    changing comment and variable names in main.sh

--- main.sh
+++ main.sh
@@ -7,37 +7,39 @@ filename=$1
 #3. return the chosen value
 function search()
 {
-    local string=$1
-    local result=""
-    local array=()
+    local search_string=$1
+    local search_result=""
+    local array_of_result=()
 
-    # read filename line by line using grepc and add all the result to array
+    # read filename line by line using grep and add all the result to array
     while read -r; do
-      result=$(grep -i "$string")
-      for i in $result; do
-        array+=("$i")
+      search_result=$(grep -i "$search_string")
+      for i in $search_result; do
+        array_of_result+=("$i")
       done
     done < "$filename"
 
-    #
+    #print the options
     counter=1
-    for value in "${array[@]}"; do
+    for value in "${array_of_result[@]}"; do
       echo "$counter) $value"
       let "counter=($counter+1)"
     done
     let "counter=($counter-1)"
 
+    # the user need to choose a number between
     read -p "Please choose a number  between 1 to $counter: " number
-     if [[ $number -le $counter ]] && [[ $number -ne 0 ]]; then
+    if [[ $number -le $counter ]] && [[ $number -ne 0 ]]; then
 
-     let number=$number-1
-     echo "${array[$number]}"
-     else
-        echo "Chosed wrong option "
-     fi
+      let number=$number-1
+      echo "${array_of_result[$number]}"
+    else
+      echo " wrong option "
+    fi
 
 
 }
+
 
 ###################################MAIN#########################################
 search "rv"
