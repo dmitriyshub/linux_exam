540b38b2b20d5fe9a3351a566713b57c63f87991
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-12 16:19:00 +0300

    adiing while loop

--- search.sh
+++ search.sh
@@ -10,7 +10,9 @@ function search()
     local search_string=$1
     local search_result=""
     local array_of_result=()
-
+    local number=0
+    local counter=1
+    local true=1
     # read filename line by line using grep and add all the result to array
     while read -r; do
       search_result=$(grep -i "$search_string")
@@ -19,33 +21,35 @@ function search()
       done
     done < "$filename"
 
-    #print the options
+
+    true=1
+    while [[ $true -eq 1 ]] ; do
     counter=1
+    number=0
     for value in "${array_of_result[@]}"; do
       echo "$counter) $value"
       let "counter=($counter+1)"
     done
     let "counter=($counter-1)"
 
-    # the user need to choose a number between
-    read -p "Please choose a number  between 1 to $counter: " number
+    read -rp "Please choose a number  between 1 to $counter: " number
     if [[ $number -le $counter ]] && [[ $number -ne 0 ]]; then
 
       let number=$number-1
-      echo "${array_of_result[$number]}"
+      true=0
     else
-      echo " wrong option "
+      echo "Please chose again: "
     fi
-
+    done
+    echo "${array_of_result[$number]}"
 
 }
 
+###################################MAIN##########################################
 
-###################################MAIN#########################################
 search "rv"
 
-
-
-
-
+: '
+local varname=$2
+'
 
