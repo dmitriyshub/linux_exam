6b5ee903cafeb15d36da99df1853a926b423e8db
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-27 14:08:01 +0300

    search.sh change

--- search.sh
+++ search.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 filename=$1
 
-filename=$1
 #1. function gets a string as argument
 #2. read the $filename global variable and find all the options
 #3. return the chosen value
@@ -14,14 +13,15 @@ function search()
     local counter=1
     local true=1
     # read filename line by line using grep and add all the result to array
-    while read -r; do
+    while read -r ; do
       search_result=$(grep -i "$search_string")
+
       for i in $search_result; do
         array_of_result+=("$i")
       done
     done < "$filename"
 
-
+    #while loop until the user chose the correct option
     true=1
     while [[ $true -eq 1 ]] ; do
     counter=1
@@ -31,7 +31,7 @@ function search()
       let "counter=($counter+1)"
     done
     let "counter=($counter-1)"
-
+    # prompt for user
     read -rp "Please choose a number  between 1 to $counter: " number
     if [[ $number -le $counter ]] && [[ $number -ne 0 ]]; then
 
@@ -42,12 +42,19 @@ function search()
     fi
     done
     echo "${array_of_result[$number]}"
-
+    final_result="${array_of_result[$number]}"
 }
 
 ###################################MAIN##########################################
 
 search "rv"
+echo $final_result
+#global_result="$( search "rv" )"
+#
+#echo $global_result
+
+
+
 
 : '
 local varname=$2
